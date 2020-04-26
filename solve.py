4026f044308be666432d4d29c1c0c02334863cc1
joel <itssme3000@gmail.com>
2020-04-26 23:42:43 +0200

    implemented new features for solver.py

--- solve.py
+++ solve.py
@@ -4,13 +4,20 @@ call this program like:
 > python3 generator.py --raw | python3 solver.py
 """
 
+
 from main import parse_maze
-from solver.fastImpl.solverIterativ import main as dijkstra
+
+from solver.curses import solverIterativ_curses
+from solver.curses import solverIterativAstar_curses
+
+from solver.fastImpl import solverIterativ
+from solver.fastImpl import solverIterativAstar
 
 import sys
+import argparse
 
 
-def main():
+def main(algo, use_curses, loop_delay, loop_delay_final):
     data = sys.stdin.read().split("\n")
 
     if not data[-1]:
@@ -18,8 +25,27 @@ def main():
 
     maze, start_pos, end_pos = parse_maze(data)
 
-    dijkstra(maze, start_pos, end_pos, True)
+    if algo == "astar":
+        if use_curses:
+            solverIterativAstar_curses.main(maze, start_pos, end_pos, loop_delay, loop_delay_final)
+        else:
+            solverIterativAstar.main(maze, start_pos, end_pos, print_maze=True)
+    elif algo == "dijkstra":
+        if use_curses:
+            solverIterativ_curses.main(maze, start_pos, end_pos, loop_delay, loop_delay_final)
+        else:
+            solverIterativ.main(maze, start_pos, end_pos, print_maze=True)
+    else:
+        raise Exception("No algorithm implementation named '" + str(algo) + "'")
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Solve mazes")
+    parser.add_argument("--algo", default="astar", type=str)
+    parser.add_argument("--maze", default="maze0.txt", type=str)
+    parser.add_argument("--curses", action="store_true")
+    parser.add_argument("--delay", default=0, type=float)
+    parser.add_argument("--delay_final", default=0, type=float)
+    args = parser.parse_args()
+
+    main(args.algo.lower(), args.curses, args.delay, args.delay_final)
